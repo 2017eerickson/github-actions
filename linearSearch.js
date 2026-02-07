80490c8ade705b00561167548b413870b9c3fbd1
ericka <2017eerickson@gmail.com>
2026-02-07 05:52:44 -0500

    passed jest test inside dev branch

--- linearSearch.js
+++ linearSearch.js
@@ -1,9 +1,37 @@
+/*
+DATA: 
+  an arr or string
+LOGIC:
+  search through iterable in order one by one
+  stopping at the first instances index
+INPUT:
+  search term and iterable
+OUTPUT:
+  index of the first instance
+*/
+
 function linearSearch(searchTerm, arr) {
+  for( let i = 0; i < arr.length; i++){
+    if (arr[i] == searchTerm){
+      return i;
+    }
+  }
   return undefined;
 }
+let nums = [1,3,4,4]
+// console.log(linearSearch("a","caop"))
 
 function globalLinearSearch(searchTerm, arr) {
-  return [];
+  let allIndex = [] ;
+  for( let i = 0; i < arr.length; i++){
+    if(arr[i] == searchTerm){
+      allIndex.push(i);
+      }
+    } 
+  return allIndex
 }
+console.log(globalLinearSearch(1, [1, 2, 3]
+  
+))
 
 module.exports = { linearSearch, globalLinearSearch };
